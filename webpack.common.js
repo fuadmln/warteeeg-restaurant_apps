6a2e9b96bfa7f887d10e59c73efaca22169ff144
Fuad Maulana <fuadm03@gmail.com>
2021-06-14 15:00:55 +0700

    setting webpack favicon plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -44,6 +47,11 @@ module.exports = {
           to: path.resolve(__dirname, 'dist/'),
         },
       ],
+    }),
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, 'src/public/w.png'),
+      outputPath: '/images/favicons',
+      prefix: 'favicons/',
     }),
   ],
 };
