b2428b4f0df0a479af017a415bb0ab75a2deb507
Fuad Maulana <fuadm03@gmail.com>
2021-06-16 01:08:50 +0700

    add pages view file

---
+++ src/scripts/views/pages/search.js
@@ -0,0 +1,13 @@
+const Search = {
+  async render() {
+    return `
+      <h2>Search Result Page</h2>
+    `;
+  },
+
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+
+export default Search;
