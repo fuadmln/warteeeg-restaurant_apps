e26efc0fa80f100818e92465be477eea86727677
Fuad Maulana <fuadm03@gmail.com>
2021-06-16 14:50:52 +0700

    remove unused code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,12 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import App from './views/app';
-import data from './data/DATA.json';
-import { createRestaurantTemplate } from './views/templates/restaurant-creator';
 
 // eslint-disable-next-line no-unused-vars
 const app = new App({
   button: document.querySelector('#hamburger'),
   drawer: document.querySelector('#navigationDrawer'),
   content: document.querySelector('#main'),
-});
-
-const { restaurants } = data;
-
-const exploreRestaurantElement = document.querySelector('.explore__items');
-restaurants.forEach((restaurant) => {
-  exploreRestaurantElement.innerHTML += createRestaurantTemplate(restaurant);
 });
 
 window.addEventListener('hashchange', () => {
