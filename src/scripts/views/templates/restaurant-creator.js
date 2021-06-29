acb9ba4060a9833ee093b2d21fb5af175e7d0bdb
Fuad Maulana <fuadm03@gmail.com>
2021-06-29 13:47:11 +0700

    add function: createRestaurantDetail, createMenuSection, createReviewSection

--- src/scripts/views/templates/restaurant-creator.js
+++ src/scripts/views/templates/restaurant-creator.js
@@ -14,4 +14,127 @@ const createRestaurantTemplate = (restaurant) => `
   </div>
 `;
 
-export { createRestaurantTemplate };
+const createRestaurantDetail = (restaurantData) => {
+  const { restaurant } = restaurantData;
+
+  // categories to string, ex: 'Italia, Modern'
+  const categories = () => {
+    const arr = [];
+    restaurant.categories.forEach((category) => {
+      arr.push(category.name);
+    });
+    return arr.join(', ');
+  };
+
+  return `
+  <div class="restaurant">
+    <img src="https://restaurant-api.dicoding.dev/images/medium/${restaurant.pictureId}" alt="nama res">
+    <div class="res__info">
+      <h2 class="res__name">${restaurant.name}</h2>
+      <p class="res__desc">${restaurant.description}</p>
+    </div>
+    <ul class="info-list">
+      <li>
+          <span class="label">Rating</span>
+          <span class="colon">:</span>
+          <span class="value">${restaurant.rating} <span class="star"
+                aria-label="rating star">★</span></span>
+      </li>
+      <li>
+          <span class="label">City</span>
+          <span class="colon">:</span>
+          <span class="value">${restaurant.city}</span>
+      </li>
+      <li>
+          <span class="label">Address</span>
+          <span class="colon">:</span>
+          <span class="value">${restaurant.address}</span>
+      </li>
+      <li>
+          <span class="label">Category</span>
+          <span class="colon">:</span>
+          <span class="value">${categories()}</span>
+      </li>
+    </ul>
+  </div>
+`;
+};
+
+const createMenuSection = (restaurantData) => {
+  const { restaurant } = restaurantData;
+
+  const foodList = () => {
+    let foods = '';
+    restaurant.menus.foods.forEach((food) => {
+      foods += `<li>${food.name}</li>`;
+    });
+    return foods;
+  };
+
+  const drinkList = () => {
+    let drinks = '';
+    restaurant.menus.drinks.forEach((drink) => {
+      drinks += `<li>${drink.name}</li>`;
+    });
+    return drinks;
+  };
+
+  return `
+    <h2>Menus</h2>
+    <div class="foods">
+      <h3>Foods</h3>
+      <ul>
+        ${foodList()}
+      </ul>
+    </div>
+    <div class="drinks">
+      <h3>Drinks</h3>
+      <ul>
+        ${drinkList()}       
+      </ul>
+    </div>
+  `;
+};
+
+const createReviewSection = (restaurantData) => {
+  const { restaurant } = restaurantData;
+  const reviews = () => {
+    let reviewsElement = '';
+    restaurant.customerReviews.forEach((review) => {
+      reviewsElement += `
+      <div class="review">
+        <div class="review__name">${review.name}</div>
+        <div class="review__date">${review.date}</div>
+        <div class="review__comment">
+        ${review.review}
+        </div>
+      </div>
+      `;
+    });
+
+    return reviewsElement;
+  };
+
+  return `
+    <h2>Reviews</h2>
+    <div class="reviews">
+      ${reviews()}
+    </div>
+    <div class="add-review">
+      <h3>Add review</h3>
+      <form action="">
+        <input type="text" name="commnet-name" placeholder="your name">
+        <textarea name="review-comment" id="review-comment" rows="5"
+          placeholder="write your review about this place..."></textarea>
+        <button>Post</button>
+      </form>
+    </div>
+  `;
+};
+
+export {
+  createRestaurantTemplate,
+  createRestaurantDetail,
+  createMenuSection,
+  createReviewSection,
+};
