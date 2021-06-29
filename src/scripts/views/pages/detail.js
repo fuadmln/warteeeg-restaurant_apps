69df09bca79c089230a158770a3bdc069c2864c9
Fuad Maulana <fuadm03@gmail.com>
2021-06-29 13:46:15 +0700

    edit afterRender function

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,12 +1,31 @@
+import UrlParser from '../../routes/url-parser';
+import RestaurantSource from '../../data/restaurant-source';
+import {
+  createRestaurantDetail,
+  createMenuSection,
+  createReviewSection,
+} from '../templates/restaurant-creator';
+
 const Detail = {
   async render() {
     return `
-      <h2>Detail Page</h2>
+      <div class="detail" id="detail"></div>
+      <section class="menu" id="menu"></section>
+      <section class="review-section" id="review"></section>
     `;
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurant = await RestaurantSource.detailRestaurant(url.id);
+
+    const detailContainer = document.querySelector('#detail');
+    const menuContainer = document.querySelector('#menu');
+    const reviewContainer = document.querySelector('#review');
+
+    detailContainer.innerHTML = createRestaurantDetail(restaurant);
+    menuContainer.innerHTML = createMenuSection(restaurant);
+    reviewContainer.innerHTML = createReviewSection(restaurant);
   },
 };
 
