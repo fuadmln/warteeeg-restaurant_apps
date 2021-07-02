77ec2a8a9a241f569964c622ed9128d46f284e07
Fuad Maulana <fuadm03@gmail.com>
2021-07-02 23:42:34 +0700

    prepare function

---
+++ src/scripts/utils/cache-helper.js
@@ -0,0 +1,15 @@
+const CacheHelper = {
+  async cachingAppShell(requests) {
+
+  },
+
+  async deleteOldCache() {
+
+  },
+
+  async revalidateCache(request) {
+
+  },
+};
+
+export default CacheHelper;
