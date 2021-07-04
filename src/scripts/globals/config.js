9c6c05c7c048e96a4c6cc5c07d07f4fdac63aa83
Fuad Maulana <fuadm03@gmail.com>
2021-07-04 10:54:45 +0700

    add config for idb

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,6 +2,9 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   // CACHE_NAME: 'WarteeeegApp-V1',
   CACHE_NAME: new Date().toISOString(), // for develop
+  DATABASE_NAME: 'warteg-restaurant-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurants',
 };
 
 export default CONFIG;
