99b9e2299c206e1f84b6ae5b50223f8843b749e1
Fuad Maulana <fuadm03@gmail.com>
2021-07-04 10:32:02 +0700

    add cache name for developer mode

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
-  CACHE_NAME: 'WarteeeegApp-V1',
+  // CACHE_NAME: 'WarteeeegApp-V1',
+  CACHE_NAME: new Date().toISOString(), // for develop
 };
 
 export default CONFIG;
