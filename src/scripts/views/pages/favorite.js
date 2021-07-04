64000aad841433a0ce1ddf5305d707f2d4c16eb4
Fuad Maulana <fuadm03@gmail.com>
2021-07-04 15:07:11 +0700

    dispaly favorited restaurant

---
+++ src/scripts/views/pages/favorite.js
@@ -0,0 +1,23 @@
+import FavoriteRestaurantIdb from '../../data/favorite-restaurant-idb';
+import { createRestaurantTemplate } from '../templates/restaurant-creator';
+
+const Favorite = {
+  async render() {
+    return `
+      <section id="favorite">
+        <h2>Favorite Restaurants</h2>
+        <div class="explore__items" id="favorites"></div>
+      <section>
+    `;
+  },
+
+  async afterRender() {
+    const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
+    const exploreRestaurantElement = document.querySelector('#favorites');
+    restaurants.forEach((restaurant) => {
+      exploreRestaurantElement.innerHTML += createRestaurantTemplate(restaurant);
+    });
+  },
+};
+
+export default Favorite;
