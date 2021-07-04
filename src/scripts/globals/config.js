c99e0217660408a5d4d7cb64ac8f7153fc8009b5
Fuad Maulana <fuadm03@gmail.com>
2021-07-05 01:09:18 +0700

    add api key

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  API_KEY: '12345',
   // CACHE_NAME: 'WarteeeegApp-V1',
   CACHE_NAME: new Date().toISOString(), // for develop
   DATABASE_NAME: 'warteg-restaurant-database',
