8b813eb1a0a487ca3cf7cb4d8dd6259601c397a3
Fuad Maulana <fuadm03@gmail.com>
2021-07-04 11:01:16 +0700

    implement function

--- src/scripts/data/favoriterestaurant-idb.js
+++ src/scripts/data/favoriterestaurant-idb.js
@@ -8,3 +8,20 @@ const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
     database.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id' });
   },
 });
+
+const FavoriteRestaurantIdb = {
+  async getRestaurant(id) {
+    return (await dbPromise).get(OBJECT_STORE_NAME, id);
+  },
+  async getAllRestaurants() {
+    return (await dbPromise).getAll(OBJECT_STORE_NAME);
+  },
+  async putRestaurant(Restaurant) {
+    return (await dbPromise).put(OBJECT_STORE_NAME, Restaurant);
+  },
+  async deleteRestaurant(id) {
+    return (await dbPromise).delete(OBJECT_STORE_NAME, id);
+  },
+};
+
+export default FavoriteRestaurantIdb;
