745a419f3039a4945432ab98d283da69a116fe52
Fuad Maulana <fuadm03@gmail.com>
2021-07-08 01:10:39 +0700

    separate main.css responsive.css

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
 
